5cfd74e65bca9a62c645f994feb0d2c388cae899
hiromiick <batterydandan@gmail.com>
2023-10-03 22:55:00 +0900

    Initial Commit

---
+++ iTunesSearchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTunesSearchApp
+//
+//  Created by hiromiick on 2023/10/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTunesSearchApp/iTunesSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchAppApp.swift
+//  iTunesSearchApp
+//
+//  Created by hiromiick on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
